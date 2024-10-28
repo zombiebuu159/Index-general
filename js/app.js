16608b696a9f3e3ef59e50a45bcf8864c83317a2
zombiebuu159 <ivanespinoza0226@gmail.com>
2024-10-28 14:50:08 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function filterProjects(category) {
+    const projects = document.querySelectorAll('.project-card');
+    
+    projects.forEach(project => {
+        if (category === 'all' || project.classList.contains(category)) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+}
